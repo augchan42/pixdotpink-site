f70c557870d6c39c037a50655f518b377d72152b
Augustin Chan <aug@iterative.day>
2025-01-12 19:52:07 +0800

    update carousel remove real photos

--- src/lib/carousel-items.ts
+++ src/lib/carousel-items.ts
@@ -1,123 +1,95 @@
 // Auto-generated file - DO NOT EDIT
 export const carouselItems = [
   {
-    src: "/images/carousel/hosermage_1970s_screen_grab_Hong_Kong_teahouse_faceless_young_f95c110c-259f-4ccf-8c3e-fef61809dfab_2.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_29010b16-e249-4b24-8aa8-9f66ebf30e38_3.png",
     alt: "Carousel Image 1"
   },
   {
-    src: "/images/carousel/hosermage_1970s_screen_grab_classic_Hong_kong_teahouse_tech_n_9d5ab325-317f-4724-ae11-24775dd77f06_0.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_5c3f2854-7dd7-4af1-8e87-300481bf47e0_2.png",
     alt: "Carousel Image 2"
   },
   {
-    src: "/images/carousel/hosermage_1970s_screen_grab_quaint_teahouse_terminal_tech_noi_b112a4ea-8afb-48a6-80a2-f34c5615d28a_2.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_6da71039-39f0-4cf8-87a9-6cc34f133929_1.png",
     alt: "Carousel Image 3"
   },
   {
-    src: "/images/carousel/hosermage_1970s_screen_grab_quaint_teahouse_terminal_tech_noi_cd973280-d0f0-46b7-b937-ecf410287242_1.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_db96a4a9-775e-40bb-a07b-c905720063f5_2 (2).png",
     alt: "Carousel Image 4"
   },
   {
-    src: "/images/carousel/hosermage_1970s_screen_grab_young_girl_tech_noir_i-ching_comp_2d966323-c287-40ca-97db-12b2a530ed0d_1.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__0461126a-60a9-4706-8322-3dda17ae7a98_0.png",
     alt: "Carousel Image 5"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_29010b16-e249-4b24-8aa8-9f66ebf30e38_3.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__0461126a-60a9-4706-8322-3dda17ae7a98_1.png",
     alt: "Carousel Image 6"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_5c3f2854-7dd7-4af1-8e87-300481bf47e0_2.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__0461126a-60a9-4706-8322-3dda17ae7a98_3.png",
     alt: "Carousel Image 7"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_6da71039-39f0-4cf8-87a9-6cc34f133929_1.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__136083b3-3839-4afa-9848-1fc855485237_3.png",
     alt: "Carousel Image 8"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_lin_d_db96a4a9-775e-40bb-a07b-c905720063f5_2 (2).png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_with__196562ab-df66-43c1-88dd-47a57546490f_0.png",
     alt: "Carousel Image 9"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__0461126a-60a9-4706-8322-3dda17ae7a98_0.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_with__9c3441b3-8fa6-4aac-8145-feb899f1a700_2.png",
     alt: "Carousel Image 10"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__0461126a-60a9-4706-8322-3dda17ae7a98_1.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_with__f14b72be-6094-4653-995b-9917a81b12f4_1.png",
     alt: "Carousel Image 11"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__0461126a-60a9-4706-8322-3dda17ae7a98_3.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_lin_daiyu_as_reincar_4a3e164e-8e23-4015-8984-a0d66d6c63ee_2.png",
     alt: "Carousel Image 12"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_pink__136083b3-3839-4afa-9848-1fc855485237_3.png",
+    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_street_urchin_spirit_a5c51d46-48e5-4a34-879c-30f79f06811d_3.png",
     alt: "Carousel Image 13"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_with__196562ab-df66-43c1-88dd-47a57546490f_0.png",
+    src: "/images/carousel/hosermage_Neon_signs_meets_traditional_symbols_spiritfairycle_2d25372c-9b35-4c12-b95b-e88808adc506_2.png",
     alt: "Carousel Image 14"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_with__9c3441b3-8fa6-4aac-8145-feb899f1a700_2.png",
+    src: "/images/carousel/hosermage_Street_Fighter_IIIs_ink_style_street_urchin_spiritf_7477e0b7-3da5-4f84-8e85-784b6685efa8_3.png",
     alt: "Carousel Image 15"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_black_and_gold_with__f14b72be-6094-4653-995b-9917a81b12f4_1.png",
+    src: "/images/carousel/hosermage_manhua_street_urchin_spiritfairyclever_Pix_a_little_a1c46653-9c23-4172-9ec3-e3a14acf0773_1.png",
     alt: "Carousel Image 16"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_lin_daiyu_as_reincar_4a3e164e-8e23-4015-8984-a0d66d6c63ee_2.png",
+    src: "/images/carousel/hosermage_old_Hong_Kong_teahouse_young_girl_from_the_back_old_7b410f5c-7fe5-4a53-b750-c31792a7a789_2.png",
     alt: "Carousel Image 17"
   },
   {
-    src: "/images/carousel/hosermage_Jet_Set_Radios_urban_aesthetic_street_urchin_spirit_a5c51d46-48e5-4a34-879c-30f79f06811d_3.png",
+    src: "/images/carousel/hosermage_old_Hong_Kong_teahouse_young_girl_from_the_back_old_aa75655b-f372-4ae9-8b9d-d41de33f943f_0.png",
     alt: "Carousel Image 18"
   },
   {
-    src: "/images/carousel/hosermage_Neon_signs_meets_traditional_symbols_spiritfairycle_2d25372c-9b35-4c12-b95b-e88808adc506_2.png",
+    src: "/images/carousel/hosermage_old_Hong_Kong_teahouse_young_girl_old_computer_term_063211cd-3d13-4bb4-b864-7e219034ce91_1.png",
     alt: "Carousel Image 19"
   },
   {
-    src: "/images/carousel/hosermage_Street_Fighter_IIIs_ink_style_street_urchin_spiritf_7477e0b7-3da5-4f84-8e85-784b6685efa8_3.png",
+    src: "/images/carousel/hosermage_old_hong_kong_teahouse_Jet_Set_Radios_urban_aesthet_6a8ed657-48c3-41ee-8b80-a46a64b19f87_1.png",
     alt: "Carousel Image 20"
   },
   {
-    src: "/images/carousel/hosermage_httpss.mj.runDlFVPqkPBP4_httpss.mj.runsEX91Y-XTus_o_5f2f28b8-ce36-46fd-8e46-eb8f06ad40e7_1.png",
+    src: "/images/carousel/hosermage_old_hong_kong_teahouse_Jet_Set_Radios_urban_aesthet_aa5e6fa6-65b0-4ba8-ad12-fb83f4058bda_1.png",
     alt: "Carousel Image 21"
   },
   {
-    src: "/images/carousel/hosermage_httpss.mj.runDlFVPqkPBP4_httpss.mj.runsEX91Y-XTus_o_5f2f28b8-ce36-46fd-8e46-eb8f06ad40e7_3.png",
+    src: "/images/carousel/hosermage_old_hong_kong_teahouse_over_the_shoulder_Jet_Set_Ra_d2599865-ba00-44c4-bea4-c7d969089e81_1.png",
     alt: "Carousel Image 22"
   },
   {
-    src: "/images/carousel/hosermage_manhua_street_urchin_spiritfairyclever_Pix_a_little_a1c46653-9c23-4172-9ec3-e3a14acf0773_1.png",
-    alt: "Carousel Image 23"
-  },
-  {
-    src: "/images/carousel/hosermage_old_Hong_Kong_teahouse_young_girl_from_the_back_old_7b410f5c-7fe5-4a53-b750-c31792a7a789_2.png",
-    alt: "Carousel Image 24"
-  },
-  {
-    src: "/images/carousel/hosermage_old_Hong_Kong_teahouse_young_girl_from_the_back_old_aa75655b-f372-4ae9-8b9d-d41de33f943f_0.png",
-    alt: "Carousel Image 25"
-  },
-  {
-    src: "/images/carousel/hosermage_old_Hong_Kong_teahouse_young_girl_old_computer_term_063211cd-3d13-4bb4-b864-7e219034ce91_1.png",
-    alt: "Carousel Image 26"
-  },
-  {
-    src: "/images/carousel/hosermage_old_hong_kong_teahouse_Jet_Set_Radios_urban_aesthet_6a8ed657-48c3-41ee-8b80-a46a64b19f87_1.png",
-    alt: "Carousel Image 27"
-  },
-  {
-    src: "/images/carousel/hosermage_old_hong_kong_teahouse_Jet_Set_Radios_urban_aesthet_aa5e6fa6-65b0-4ba8-ad12-fb83f4058bda_1.png",
-    alt: "Carousel Image 28"
-  },
-  {
-    src: "/images/carousel/hosermage_old_hong_kong_teahouse_over_the_shoulder_Jet_Set_Ra_d2599865-ba00-44c4-bea4-c7d969089e81_1.png",
-    alt: "Carousel Image 29"
-  },
-  {
     src: "/images/carousel/hosermage_old_hong_kong_teahouse_over_the_shoulder_Jet_Set_Ra_d2599865-ba00-44c4-bea4-c7d969089e81_3.png",
-    alt: "Carousel Image 30"
+    alt: "Carousel Image 23"
   }
 ] as const;
